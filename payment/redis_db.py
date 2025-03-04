da1b8b19d8cea66ed7752087ba613837dc7fe478
Gijs Margadant <g.e.margadant@student.tudelft.nl>
2025-03-04 11:52:31 +0100

    Added error messages to redis_db

--- payment/redis_db.py
+++ payment/redis_db.py
@@ -2,18 +2,27 @@ import os
 
 from redis.asyncio import Redis
 
-db: Redis
+db: Redis | None = None
 
 
 async def get(key: str):
+    if db is None:
+        raise RuntimeError("The Redis client has not been initialized. Call init() first.")
+
     return await db.get(key)
 
 
 async def set(key: str, value: bytes):
+    if db is None:
+        raise RuntimeError("The Redis client has not been initialized. Call init() first.")
+
     await db.set(key, value)
 
 
 async def multi_set(kv_pairs: dict[str, bytes]):
+    if db is None:
+        raise RuntimeError("The Redis client has not been initialized. Call init() first.")
+
     await db.mset(kv_pairs)
 
 
@@ -28,4 +37,5 @@ def init():
 
 
 async def close():
-    await db.close()
+    if db is not None:
+        await db.close()
